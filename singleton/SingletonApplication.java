f96ad510bb0a2f26814b477c3aeed69281b81b6f
Jérémy <jeremy.perrouault@ascadis.fr>
2025-03-24 13:21:13 +0000

    JOUR 1 - SINGLETON (2 PAS BIEN MAIS POSSIBLE)

--- singleton/SingletonApplication.java
+++ singleton/SingletonApplication.java
@@ -1,10 +1,21 @@
+import java.lang.reflect.Constructor;
+
 public class SingletonApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        // BIEN
         DbConnection dbc1 = DbConnection.getInstance();
         DbConnection dbc2 = DbConnection.getInstance();
+
+        // PAS BIEN
+        Constructor<DbConnection> ctor = DbConnection.class.getDeclaredConstructor();
+
+        ctor.setAccessible(true);
+
+        DbConnection dbc3 = ctor.newInstance();
 
         dbc1.connect();
 
         System.out.println(dbc1 == dbc2);
+        System.out.println(dbc1 == dbc3);
     }
 }
